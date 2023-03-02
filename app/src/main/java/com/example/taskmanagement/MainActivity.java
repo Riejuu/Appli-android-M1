63c43c650ae2c46fd031d1e9a29d023e775f429d
esteb <esteban.jouan@etu.univ-tours.fr>
2023-03-02 16:50:19 +0100

    barre des taches rajoutée

--- app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.taskmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+       //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_barre_des_taches, new BarreDesTaches()).commit();
+
+
+
+
+
     }
 }
