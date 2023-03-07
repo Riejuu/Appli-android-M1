6a6cb50261d4a0cb4a1fda0a9962621690d3f0e0
esteb <esteban.jouan@etu.univ-tours.fr>
2023-03-07 12:39:25 +0100

    ajout d'un calendrier

--- app/src/main/java/com/example/taskmanagement/CalendrierFragment.java
+++ app/src/main/java/com/example/taskmanagement/CalendrierFragment.java
@@ -1,12 +1,24 @@
 package com.example.taskmanagement;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,6 +58,7 @@ public class CalendrierFragment extends Fragment {
         return fragment;
     }
 
+    //la vue se crée apres le on create, donc ca crée des null pointer exception si on met le code du calendrier dans onCreate et non dans onViewCreated
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +66,52 @@ public class CalendrierFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        CalendarView calendarView = (CalendarView) getActivity().findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // Faites quelque chose lorsque l'utilisateur sélectionne une nouvelle date dans le calendrier
+                System.out.println("hey salut");
+            }
+        });
+
+
+        // Récupération du ContentResolver
+        ContentResolver cr = getActivity().getContentResolver();
+
+        // Création d'un nouvel événement
+        ContentValues values = new ContentValues();
+        int date = 5;
+        values.put(CalendarContract.Events.DTSTART, date);
+        values.put(CalendarContract.Events.DTEND, date);
+        values.put(CalendarContract.Events.TITLE, "Mon nouvel événement");
+        values.put(CalendarContract.Events.DESCRIPTION, "Description de l'événement");
+        values.put(CalendarContract.Events.CALENDAR_ID, 1);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID());
+
+        // Ajout de l'événement au calendrier
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+        // Affichage de la confirmation
+        //Toast.makeText(getActivity(), "Evénement ajouté au calendrier", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(uri);
+        startActivity(intent);
+
+
+
+
+
     }
 
     @Override
